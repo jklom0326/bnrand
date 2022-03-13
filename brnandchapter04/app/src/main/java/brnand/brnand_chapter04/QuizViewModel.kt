a24bb11649136f30695ff0acf1febcb18051bedc
Sangwan Han <jklom0326@gmail.com>
2022-03-13 23:23:22 +0900

    view model 사용

--- brnandchapter04/app/src/main/java/brnand/brnand_chapter04/QuizViewModel.kt
+++ brnandchapter04/app/src/main/java/brnand/brnand_chapter04/QuizViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.ViewModel
 private const val TAG = "QuizViewModel"
 
 class QuizViewModel:ViewModel() {
+    //초기화 블록 이 코드는 클랫 ㅡ인스턴스 생성시에 자동으로 실행된다.
     init {
         Log.d(TAG,"ViewModel instance created")
     }
