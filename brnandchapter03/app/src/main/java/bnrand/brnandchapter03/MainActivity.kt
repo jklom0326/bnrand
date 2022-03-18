549c6de6b799ad8300947905a9c6448631025704
Sangwan Han <jklom0326@gmail.com>
2022-03-18 22:32:39 +0900

    challenge clear

--- brnandchapter03/app/src/main/java/bnrand/brnandchapter03/MainActivity.kt
+++ brnandchapter03/app/src/main/java/bnrand/brnandchapter03/MainActivity.kt
@@ -22,6 +22,8 @@ class MainActivity : AppCompatActivity() {
         Question(R.string.question_asia, true, false),
     )
     private var currentIndex = 0
+    private var count = 0
+    private var correct = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,26 +33,49 @@ class MainActivity : AppCompatActivity() {
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
         nextButton = findViewById(R.id.next_button)
-        previousButton = findViewById()
+        previousButton = findViewById(R.id.prev_button)
         questionTextView = findViewById(R.id.question_text_view)
 
         trueButton.setOnClickListener {
             // 버튼클릭의 응답을 여기서 처리한다.
+            count++
+            trueButton.isEnabled = false
+            falseButton.isEnabled = false
+            questionBank[currentIndex].answered = true
             checkAnswer(true)
         }
         falseButton.setOnClickListener {
             // 버튼클릭의 응답을 여기서 처리한다.
+            count++
+            trueButton.isEnabled = false
+            falseButton.isEnabled = false
+            questionBank[currentIndex].answered = true
             checkAnswer(false)
         }
         // 다음버튼을 누르면 리스트에 있는 다음 문제가 나온다
         nextButton.setOnClickListener {
-            currentIndex =(currentIndex + 1) % questionBank.size
+            val score = String.format("%.2f",(correct.toDouble() / questionBank.size) * 100)
+            currentIndex = (currentIndex + 1) % questionBank.size
             isAnswered(currentIndex)
+            if (count == questionBank.size)
+                Toast.makeText(this, "정답률: ${score}%", Toast.LENGTH_SHORT).show()
+            updateQuestion()
+        }
+        previousButton.setOnClickListener {
+            if (currentIndex > 0) {
+                currentIndex = currentIndex - 1
+                updateQuestion()
+            } else  {
+                currentIndex = questionBank.size - 1
+                updateQuestion()
+            }
+        }
+        questionTextView.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
             updateQuestion()
         }
         updateQuestion()
     }
-
 
     private fun updateQuestion(){
         val questionTextResId = questionBank[currentIndex].tesxtResId
@@ -61,6 +86,7 @@ class MainActivity : AppCompatActivity() {
         val correctAnswer = questionBank[currentIndex].answer
 
         val messageResId = if (userAnswer == correctAnswer) {
+            correct++
             R.string.correct_toast
         } else {
             R.string.incorrect_tost
