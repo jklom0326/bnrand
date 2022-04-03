b2dcc63a6eb3fe283db22eace6b7c1da7e7c9662
Sangwan Han <jklom0326@gmail.com>
2022-04-04 03:12:59 +0900

    CrimeFragment

---
+++ CriminalIntent/app/src/main/java/bnrand/part2/CrimeFragment.kt
@@ -0,0 +1,30 @@
+package bnrand.part2
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import androidx.fragment.app.Fragment
+
+class CrimeFragment: Fragment() {
+    private lateinit var crime: Crime
+    private lateinit var titleField: EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        crime = Crime()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_crime, container, false)
+
+        titleField = view.findViewById(R.id.crime_title)
+
+        return view
+    }
+}
